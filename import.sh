cd970112f8c4f8d6326e483d69b7355a61043b1c
Raimon Grau <raimonster@gmail.com>
2017-12-29 14:49:53 +0000

    Use ruby to parse the html correctly.
    * removing pup. It's too limited so grouping data together is too
      clunky.

--- import.sh
+++ import.sh
@@ -1,3 +1,5 @@
+#!/bin/zsh
+rm HttpLuaModule.wiki*
 rm README.markdown*
 rm docSet.dsidx
 rm functions.sql
@@ -6,32 +8,15 @@ rm -fr HttpLuaModule.html
 rm -fr HttpLuaModule.docset/
 
 # get the page
-wget https://github.com/openresty/lua-nginx-module/blob/master/README.markdown
-
-#clean it
-sed -i '1,/article class="markdown-body entry-content"/d' README.markdown
-
-# Prepare valid html page
-echo '<html><body>' > HttpLuaModule.html
-cat README.markdown >>HttpLuaModule.html
-
-# Create docset db
-sqlite3 docSet.dsidx  'create table searchIndex(id INTEGER PRIMARY KEY, name TEXT, type TEXT, path TEXT);  CREATE UNIQUE INDEX anchr  ON searchIndex (name, type, path);'
-
-
-# Fetch functions and categories
-grep 'class="anchor"' README.markdown| grep '<h2>' | sed -e 's!^.*href="\([^"]*\).*</a>\(.*\)</h2>!sqlite3 docSet.dsidx "insert into searchIndex(name,type,path) VALUES (\\"\2\\", \\"Function\\", \\"HttpLuaModule.html\1\\" )"!' > functions.sql
-grep 'class="anchor"' README.markdown| grep '<h1>' | sed -e 's!^.*href="\([^"]*\).*</a>\(.*\)</h1>!sqlite3 docSet.dsidx "insert into searchIndex(name,type,path) VALUES (\\"\2\\", \\"Category\\", \\"HttpLuaModule.html\1\\" )"!' > categories.sql
-
-# Insert into sqlite db
-sh functions.sql
-sh categories.sql
+wget https://raw.githubusercontent.com/openresty/lua-nginx-module/master/doc/HttpLuaModule.wiki
+perl wiki2markdown.pl HttpLuaModule.wiki >HttpLuaModule.markdown
+pandoc -o HttpLuaModule.html HttpLuaModule.markdown
+ruby parse.rb HttpLuaModule.html
 
 # Create the valid docset structure
 mkdir -p HttpLuaModule.docset/Contents/Resources/Documents
 mv docSet.dsidx HttpLuaModule.docset/Contents/Resources/
 mv HttpLuaModule.html HttpLuaModule.docset/Contents/Resources/Documents/
-
 
 # Alternative way, using xmlstarlet
 # xmlstarlet sel -T -t -m "/html/body/h2/a[@class='anchor']/parent::*"  -v self::h2 -o " => " -m "a" -v @href -n  README.markdown
